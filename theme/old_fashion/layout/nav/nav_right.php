c90368c83749c5335872e8ee3ddb7274ff500f93
virgofreedom <rattana.neak@gmail.com>
2016-06-05 13:42:03 -0700

    creation the repo

---
+++ theme/old_fashion/layout/nav/nav_right.php
@@ -0,0 +1,3 @@
+<div class="right">
+      <span class="username">Souris</span> <a href="#" class="white-color"><i class="fa fa-power-off fa-lg"></i></a>
+</div>
---
+++ theme/old_fashion/layout/nav_bar.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+<?php
+#nav_bar.php will store the top navigation part
+//here the left top navigation
+include 'nav/nav_left.php';
+//here the center top navigation
+include 'nav/nav_center.php';
+//here we include the right top navigation
+include 'nav/nav_right.php';
+
+?>
+</nav> <!-- end navigation bar -->
