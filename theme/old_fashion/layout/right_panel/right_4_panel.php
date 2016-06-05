5b2e62a04a9c599eb864bd2857b310e1853455bf
virgofreedom <rattana.neak@gmail.com>
2016-06-05 16:50:12 -0700

    add some function in common file

--- theme/old_fashion/layout/right_panel/right_4_panel.php
+++ theme/old_fashion/layout/right_panel/right_4_panel.php
@@ -1,4 +1,4 @@
-<!-- Left panel in main container -->
+<!-- Right panel in main container -->
 <div class="small-12 medium-4 columns">
       <div class="small-12 columns console-header">
           <!--panel label/header -->
@@ -8,8 +8,8 @@
           <!-- panel's container
           we can add more items like : navigation etc...
           -->
-          <?php include 'left_panel/index.php'; ?>      
+          <?php include 'left_panel/contain.php'; ?>      
       </div>
 
 </div>
-<!-- End left panel in main container -->
+<!-- End Right panel in main container -->
---
+++ view/login.php
@@ -0,0 +1,16 @@
+<?php include 'include/head.php'; ?>
+	<div class="container">
+		<div class="small-12 medium-6 medium-offset-3 columns">
+			<div class="small-12 columns login-head">
+				FPanel Login
+			</div>
+			<div class="small-12 columns login-form">
+				<form action="">
+					<input type="email" placeholder="Email" name="email">
+					<input type="password" placeholder="Password" name="pass">
+					<input type="submit" name="submit" class="flat-purple right">
+				</form>
+			</div>
+		</div>
+	</div>
+<?php include 'include/foot.php'; ?>
